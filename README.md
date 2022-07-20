de72fdb3a089347855bea8382febcc40e147e31b
Dana Huffman <dana.huffman@yahoo.com>
2022-07-20 13:34:39 -0700

    Passed test 1

---
+++ README.md
@@ -0,0 +1,11 @@
+# Lab - Class 03
+
+## Project: Madlib Client
+
+### Author: Dana Huffman
+
+### Links and Resources
+
+https://careerkarma.com/blog/python-string-strip/#:~:text=The%20Python%20strip()%20method,you%20have%20specified%20to%20remove.&text=This%20example%20removes%20all%20the,space%20characters%20in%20our%20string.
+
+### Setup
---
+++ madlib.py
@@ -0,0 +1,19 @@
+print("""
+** Welcome! I'm glad you're here!
+** I've got a fun game for you today...
+** Madlibs!
+** In this game, all you need to do is answer my prompts and I'll give you back a fun scenario!
+** Are you ready?
+** Let's start!
+""")
+
+def read_template(f):
+    with open(f) as file:
+        text = file.read()
+    return text.strip()
+
+def parse_template():
+    return 0
+
+def merge():
+    return 0
